f27c89b68b6670e1e302814de7689cdc488e183f
Sergey Dychko <dychkosergey@gmail.com>
2021-11-02 18:41:02 +0200

    home page added

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+
+
+let btnBurger = document.querySelector('.burger');
+let menu = document.querySelector('.header__nav');
+
+btnBurger.addEventListener('click',function (e){
+    btnBurger.classList.toggle('open')
+    menu.classList.toggle('open')
+})
