c46eda3a7cbf001a9cad5c29108033693493e71d
Sergey Dychko <dychkosergey@gmail.com>
2021-11-03 20:01:18 +0200

    start developing custom select

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,19 @@
 
 let btnBurger = document.querySelector('.burger');
 let menu = document.querySelector('.header__nav');
+let dropdowns = document.querySelectorAll('.dropdown');
 
 btnBurger.addEventListener('click',function (e){
     btnBurger.classList.toggle('open')
     menu.classList.toggle('open')
 })
+
+dropdowns.forEach(dropdown=>{
+    dropdown.addEventListener('click',function (e){
+        dropdown.classList.toggle('open');
+    })
+
+})
+
 
 
