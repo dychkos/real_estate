05cb0a7d80795c69dc1b0d4224bc252bad23d18d
Sergey Dychko <dychkosergey@gmail.com>
2021-11-04 13:09:00 +0200

    added custom select

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,57 @@
 
+class Select {
+    constructor(selector) {
+        this.$el = selector;
+        this.#setup();
+    }
+
+
+    #setup() {
+        this.clickHandler = this.clickHandler.bind(this);
+        this.$el.addEventListener('click', this.clickHandler);
+    }
+
+    clickHandler(event) {
+        const {type} = event.target.dataset;
+
+        if (!type) {
+            this.toggle()
+        } else if (type === 'item') {
+            const selectedItem = {
+                id:event.target.dataset.id,
+                value:event.target.textContent
+            }
+            this.select(selectedItem)
+        } else if (type === 'backdrop') {
+            this.close()
+        }
+    }
+
+    get isOpen() {
+        return this.$el.classList.contains('open')
+    }
+
+    select(selectedItem) {
+        this.selectedItem = selectedItem;
+        console.log(selectedItem);
+        this.$el.querySelector('.dropdown__title').textContent = selectedItem.value;
+        this.close()
+    }
+
+    toggle() {
+        this.isOpen ? this.close() : this.open();
+    }
+
+    open() {
+        this.$el.classList.add('open');
+    }
+
+    close() {
+        this.$el.classList.remove('open') ;
+    }
+
+}
+
 
 let btnBurger = document.querySelector('.burger');
 let menu = document.querySelector('.header__nav');
@@ -10,11 +63,7 @@ btnBurger.addEventListener('click',function (e){
 })
 
 dropdowns.forEach(dropdown=>{
-    dropdown.addEventListener('click',function (e){
-        dropdown.classList.toggle('open');
-    })
+   let select = new Select(dropdown);
 
 })
-
-
 
